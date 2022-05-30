c0b8b7067d9b9b6e70c2b9c4d2bced5cf2575c32
armit4ge <kelvinquida24@gmail.com>
2022-05-29 21:00:48 -0400

    Testing oauth20

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const saltRounds = 10;
+const session = require("express-session")
+const passport = require("passport")
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require("passport-local-mongoose")
+const findOrCreate = require('mongoose-findorcreate')
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 
 const app = express()
 
@@ -16,22 +21,141 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true})
+app.use(session({
+    secret: "Our little secret.",
+    resave:false,
+    saveUninitialized: false,
+    cookie:{
+        secure:true
+    }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
+
+// mongoose.set("useCreateIndex", true)
 
 //DATABASE ---------------------------------------------
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    facebookId:String
 })
 
-const User = new mongoose.model("User",userSchema)
+userSchema.plugin(passportLocalMongoose,{usernameField:"username"})
+userSchema.plugin(findOrCreate)
+
+const User = new mongoose.model("User",userSchema, )
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(function(user,done){
+    done(null,user.id)
+});
+
+passport.deserializeUser(function(id,none){
+    User.findById(id,function (err,user) {
+        done(err,user)
+    })
+});
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.CLIENT_ID_FB,
+    clientSecret: process.env.CLIENT_SECRET_FB,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //ROUTES -----------------------------------------------
 
 app.get("/",(req,res) => {
     res.render("home")
 })
+
+app.get('/auth/google',passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+  app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+      if (err){
+        console.log(err);
+      } else {
+        if (foundUsers) {
+          res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+      }
+    });
+  });
+
+app.get("/submit", function(req, res){
+    if (req.isAuthenticated()){
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+  
+  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+    // console.log(req.user.id);
+  
+    User.findById(req.user.id, function(err, foundUser){
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+})
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.route("/register")
 
@@ -41,23 +165,16 @@ app.route("/register")
 
 .post((req,res) => {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const username = req.body.username
-        const password = req.body.password
-
-        const newUser = new User({
-            email: username,
-            password: hash
-        })
-
-        newUser.save((err) => {
-            if(!err){
-                res.render("secrets")
-            }else{
-                res.send(err)
-            }
-        })
-    });  
+    User.register({username:req.body.username}, req.body.password, (err,user) => {
+        if(err){
+            console.log(err);
+            res.redirect("/register")
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            })
+        }
+    })
 
 })
 
@@ -68,21 +185,26 @@ app.route("/login")
 })
 
 .post((req,res) => {
-    const username = req.body.username
-    const password = req.body.password
-
-    User.findOne({email:username},(err,content) => {
-        if(!err){
-            if(content){
-                bcrypt.compare(password, content.password, function(err, result) {
-                    if(result === true){
-                        res.render("secrets")
-                    }
-                });
-                }
-            }
-        })
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
     })
+
+    req.login(user, (err) => {
+        if (err){
+            console.log(err)
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            })
+        }
+    })
+})
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/home");
+});
 
 //LISTENING PORTS ------------------------------------
 
